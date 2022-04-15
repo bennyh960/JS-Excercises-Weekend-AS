ad40a712cb92972ff60d62dd9945824fec35d75e
bennyh960 <bennyh960@gmail.com>
2022-04-15 11:04:25 +0300

    finished 6.4

--- Ex.6.3.js
+++ Ex.6.3.js
@@ -1,4 +1,4 @@
-// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the
 // longest possible, containing distinct letters,
 // each taken only once - coming from s1 or s2.
 // Examples:
@@ -7,3 +7,15 @@
 // longest(a, b) -> "abcdefklmopqwxy"
 // a = "abcdefghijklmnopqrstuvwxyz"
 // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz
+
+const concat = (s1, s2) => {
+    let bigWord ='';
+    for(let letter of (s1+s2)){
+        !bigWord.includes(letter) && (bigWord += letter);
+    }
+    return [...bigWord].sort().join("")
+}
+
+console.log(concat('xyaabbbccccdefww','xxxxyyyyabklmopq'));
+console.log(concat('xyaabbbccccdefww','xyaabbbccccdefww'));
+console.log(concat('abcdefghijklmnopqrstuvwxyz','abcdefghijklmnopqrstuvwxyz'));
