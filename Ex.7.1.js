0d3d269087ae26c3b8a2cc4ffb9845a4e0e1d47c
bennyh960 <bennyh960@gmail.com>
2022-04-17 09:48:05 +0300

    added

--- Ex.7.1.js
+++ Ex.7.1.js
@@ -7,3 +7,17 @@
 // where
 
 const solveMeFirst = (a, b) => a + b;
+
+// implement filter
+const filter = (arr, callback) => {
+  if (!Array.isArray(arr)) return "invalid filter arrguments";
+  const newArr = [];
+  for (let element of arr) {
+    if (callback(element)) newArr.push(element);
+  }
+  return newArr;
+};
+
+const grater = (num) => (num > 3 ? true : false);
+
+console.log(filter([1, 2, 3, 4, 5, 6, 7], grater));
